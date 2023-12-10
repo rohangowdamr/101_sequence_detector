49d2d1f777c112e1535a31c638b7d8b823b8239f
rohangowdamr <143600225+rohangowdamr@users.noreply.github.com>
2023-12-10 23:20:24 +0530

    Update overlaping_code.v

--- Mealy/overlaping_code.v
+++ Mealy/overlaping_code.v
@@ -1,32 +1,40 @@
-//Mealy overlaping sequence detector 101
+//mealy overlapping code
+module sequencedetector(
+  input x,
+  input clk,
+  input rst,
+  output reg z
+);
+  // Parameter definitions
+  parameter s0 = 0, s1 = 1, s2 = 2;
 
-module sequencedetector(input x,input clk,input rst,
-			output reg z);
-parameter s0=0,s1=1,s2=2;
-reg [1:0] PS,NS;
+  // State variables
+  reg [1:0] PS, NS;
 
-always @(posedge clk or posedge rst)
+  // State transition and output logic
+  always @(posedge clk or posedge rst)
+    if (rst)
+      PS <= s0; // Reset state to s0 on rst
+    else
+      PS <= NS; // Update state based on next state
 
-if(rst)
-PS<=s0;
-else
-PS<=NS;
-
-always@(PS,x)
-case (PS)
-s0: begin
-z=(x)?0:0;
-NS=(x)?s1:s0;
-end
-s1: begin
-z=(x)?0:0;
-NS=(x)?s1:s2;
-end
-s2: begin
-z=(x)?1:0;
-NS=(x)?s1:s0;
-end
-default:NS<=s0;
-endcase
-
+  always @(PS, x)
+    case (PS)
+      s0: begin
+        // Output logic for state s0
+        z = (x) ? 0 : 0; // Set output based on input
+        NS = (x) ? s1 : s0; // Transition to next state based on input
+      end
+      s1: begin
+        // Output logic for state s1
+        z = (x) ? 0 : 0;
+        NS = (x) ? s1 : s2;
+      end
+      s2: begin
+        // Output logic for state s2
+        z = (x) ? 1 : 0;
+        NS = (x) ? s1 : s0;
+      end
+      default: NS <= s0;
+    endcase
 endmodule
